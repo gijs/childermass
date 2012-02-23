2c7454fabf8deb71b9a46e508f5c8d1ccc6dbe37
Mike Jansen <mjansen401@gmail.com>
2012-02-22 21:04:46 -0600

    add jasmine-jquery, sinon, jquery

--- spec/javascripts/PlayerSpec.js
+++ spec/javascripts/PlayerSpec.js
@@ -35,16 +35,6 @@ describe("Player", function() {
     });
   });
 
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
   //demonstrates use of expected exceptions
   describe("#resume", function() {
     it("should throw an exception if song is already playing", function() {
