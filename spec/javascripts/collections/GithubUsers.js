79f3af91535b85c39bbed4a47d8be8669f7053ff
Mike Jansen <mjansen401@gmail.com>
2012-03-18 19:38:49 -0500

    move repos to main section of page

--- spec/javascripts/collections/GithubUsers.js
+++
@@ -1,7 +0,0 @@
-describe("Childermass.Collections.GithubUsers", function() {
-  //it("url is 'github/users'", function() {
-  //  var users = new Childermass.Collections.GithubUsers();
-
-  //  expect(users.url).toEqual('github/users');
-  //});
-});
